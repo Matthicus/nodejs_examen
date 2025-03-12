c4e687e962a506f246852f0086da7884f3b0ba22
Matthice Storms <matthice.storms@hotmail.com>
2025-03-12 11:17:58 +0100

    aanpassingen aan server

--- server.js
+++ server.js
@@ -14,7 +14,9 @@ app.use("/api/snippets", snippetRoutes);
 
 const PORT = process.env.PORT || 3000;
 
-mongoose.connect(process.env.MONGO_URI).then(() => {
-  app.listen(PORT, () => console.log(`server is running on ${PORT} xp`));
-});
-.catch(err => console.error(err));
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+  })
+  .catch((err) => console.error(err));
